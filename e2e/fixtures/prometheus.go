c1a25374124eca71974c7d201cb1f2573f1d5fab
Jaiveer Katariya <jveerkatariya@gmail.com>
2023-06-02 17:33:17 -0400

    sorted imports

--- e2e/fixtures/prometheus.go
+++ e2e/fixtures/prometheus.go
@@ -2,15 +2,17 @@ package fixtures
 
 import (
 	"fmt"
-	"sigs.k8s.io/e2e-framework/klient/k8s"
 
 	"github.com/Azure/aks-app-routing-operator/pkg/config"
 	"github.com/Azure/aks-app-routing-operator/pkg/util"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"sigs.k8s.io/e2e-framework/klient/k8s"
 )
 
 // these need to be the same as the consts in promclient/main.go
