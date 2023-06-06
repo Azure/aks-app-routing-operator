7e3004914d49472eb5f04d729f2a336ecd90c34d
Jaiveer Katariya <jveerkatariya@gmail.com>
2023-06-06 15:49:33 -0400

    why is indentation weird

--- devenv/scripts/run_public_cluster.sh
+++ devenv/scripts/run_public_cluster.sh
@@ -1,11 +1,11 @@
-	set -e
+set -e
 
-	echo "beginning public cluster tests..."
-	make clean
-	make dev CLUSTER_TYPE=public DNS_ZONE_TYPE=public
-	make push
-	make e2e
+echo "beginning public cluster tests..."
+make clean
+make dev CLUSTER_TYPE=public DNS_ZONE_TYPE=public
+make push
+make e2e
 
-	make dev CLUSTER_TYPE=public DNS_ZONE_TYPE=private
-	make push
-	make e2e
+make dev CLUSTER_TYPE=public DNS_ZONE_TYPE=private
+make push
+make e2e
