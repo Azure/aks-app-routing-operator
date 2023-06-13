28f5fe5b23502262e48dd4118da65052912062e0
Jaiveer Katariya <jveerkatariya@gmail.com>
2023-06-13 10:55:47 -0400

    bash indentation

--- devenv/scripts/run_private_cluster.sh
+++ devenv/scripts/run_private_cluster.sh
@@ -1,6 +1,6 @@
-	set -e
-	echo "beginning private cluster tests..."
-	make dev CLUSTER_TYPE=private
-	make push
-	make e2e
+set -e
+echo "beginning private cluster tests..."
+make dev CLUSTER_TYPE=private
+make push
+make e2e
 
--- devenv/scripts/run_public_cluster.sh
+++ devenv/scripts/run_public_cluster.sh
@@ -1,6 +1,6 @@
-	set -e
-	echo "beginning public cluster tests..."
-	make clean
-	make dev CLUSTER_TYPE=public
-	make push
-	make e2e
+set -e
+echo "beginning public cluster tests..."
+make clean
+make dev CLUSTER_TYPE=public
+make push
+make e2e
