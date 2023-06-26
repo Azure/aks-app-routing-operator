4e9242337e984c4e8ab0500e3a6010cb9835697f
Oliver King <kingoliver@microsoft.com>
2023-06-26 11:12:54 -0400

    finish clean_type tests

--- pkg/controller/common/clean_type_test.go
+++ pkg/controller/common/clean_type_test.go
@@ -5,7 +5,9 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -313,6 +315,36 @@ func TestRetrieverFromObjs(t *testing.T) {
 			expected:    []cleanType(nil),
 			expectedErr: nil,
 		},
+		{
+			name: "one obj one gvr one label",
+			objs: []client.Object{
+				obj(gvk1, labels1),
+			},
+			labels:      labels1,
+			expected:    []cleanType{{gvr: gvr1, labels: labels1}},
+			expectedErr: nil,
+		},
+		{
+			name:   "one obj multiple gvr one label",
+			objs:   []client.Object{obj(gvk2, labels1)},
+			labels: labels1,
+			expected: []cleanType{
+				{gvr: gvr2a, labels: labels1},
+				{gvr: gvr2b, labels: labels1},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "multiple objects",
+			objs:   []client.Object{obj(gvk1, labels1), obj(gvk2, labels2)},
+			labels: labels2,
+			expected: []cleanType{
+				{gvr: gvr1, labels: labels2},
+				{gvr: gvr2a, labels: labels2},
+				{gvr: gvr2b, labels: labels2},
+			},
+			expectedErr: nil,
+		},
 	}
 
 	for _, test := range tests {
@@ -334,4 +366,259 @@ func TestRetrieverFromObjs(t *testing.T) {
 			)
 		}
 	}
+}
+
+func TestRetrieverFromGk(t *testing.T) {
+	tests := []struct {
+		name        string
+		gks         []schema.GroupKind
+		labels      map[string]string
+		expected    []cleanType
+		expectedErr error
+	}{
+		{
+			name:        "empty gk",
+			gks:         nil,
+			labels:      nil,
+			expected:    nil,
+			expectedErr: nil,
+		},
+		{
+			name:        "one gk one gvr one label",
+			gks:         []schema.GroupKind{gvk1.GroupKind()},
+			labels:      labels1,
+			expected:    []cleanType{{gvr: gvr1, labels: labels1}},
+			expectedErr: nil,
+		},
+		{
+			name:   "one gk multiple gvrs one label",
+			gks:    []schema.GroupKind{gvk2.GroupKind()},
+			labels: labels1,
+			expected: []cleanType{
+				{gvr: gvr2a, labels: labels1},
+				{gvr: gvr2b, labels: labels1},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "multiple gks",
+			gks:    []schema.GroupKind{gvk1.GroupKind(), gvk2.GroupKind()},
+			labels: labels2,
+			expected: []cleanType{
+				{gvr: gvr1, labels: labels2},
+				{gvr: gvr2a, labels: labels2},
+				{gvr: gvr2b, labels: labels2},
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		fn := RetrieverFromGk(test.labels, test.gks...)
+		got, err := fn(testMapper{})
+		if err != test.expectedErr {
+			t.Error(
+				"For", test.name,
+				"got", err,
+				"expected", test.expectedErr,
+			)
+		}
+
+		if !reflect.DeepEqual(got, test.expected) {
+			t.Error(
+				"For", test.name,
+				"got", got,
+				"expected", test.expected,
+			)
+		}
+	}
+}
+
+func TestRetrieverEmpty(t *testing.T) {
+	got, err := RetrieverEmpty()(testMapper{})
+	require.NoError(t, err)
+	require.Equal(t, []cleanType{}, got)
+}
+
+func TestCleanTypeRetrieverAdd(t *testing.T) {
+	tests := []struct {
+		name        string
+		reciever    CleanTypeRetriever
+		addition    CleanTypeRetriever
+		expected    []cleanType
+		expectedErr error
+	}{
+		{
+			name:     "empty receiver, one add",
+			reciever: RetrieverEmpty(),
+			addition: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			expected:    addLabels([]schema.GroupVersionResource{gvr1}, labels1),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, zero add",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			addition:    RetrieverEmpty(),
+			expected:    addLabels([]schema.GroupVersionResource{gvr1}, labels1),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, multiple add",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			addition: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr2a, gvr2b}, labels2), nil
+			},
+			expected:    []cleanType{{gvr: gvr1, labels: labels1}, {gvr: gvr2a, labels: labels2}, {gvr: gvr2b, labels: labels2}},
+			expectedErr: nil,
+		},
+		{
+			name: "multiple receiver, one add",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr2a, gvr2b}, labels1), nil
+			},
+			addition: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels2), nil
+			},
+			expected:    []cleanType{{gvr: gvr2a, labels: labels1}, {gvr: gvr2b, labels: labels1}, {gvr: gvr1, labels: labels2}},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := test.reciever.Add(test.addition)(testMapper{})
+		if err != test.expectedErr {
+			t.Error(
+				"For", test.name,
+				"got", err,
+				"expected", test.expectedErr,
+			)
+		}
+
+		if !reflect.DeepEqual(got, test.expected) {
+			t.Error(
+				"For", test.name,
+				"got", got,
+				"expected", test.expected,
+			)
+		}
+	}
+}
+
+func TestCleanTypeRetrieverRemove(t *testing.T) {
+	tests := []struct {
+		name        string
+		reciever    CleanTypeRetriever
+		deletion    CleanTypeRetriever
+		opt         RemoveOpt
+		expected    []cleanType
+		expectedErr error
+	}{
+		{
+			name:     "empty receiver, one delete",
+			reciever: RetrieverEmpty(),
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			expected:    []cleanType(nil),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, zero delete",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			deletion:    RetrieverEmpty(),
+			expected:    addLabels([]schema.GroupVersionResource{gvr1}, labels1),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, one delete, overlap with default compare",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			expected:    []cleanType(nil),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, one delete, overlap with ignore labels",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			opt:         RemoveOpt{CompareStrat: IgnoreLabels},
+			expected:    []cleanType(nil),
+			expectedErr: nil,
+		},
+		{
+			name: "one receiver, one delete, overlap with ignore labels, different labels",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels1), nil
+			},
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1}, labels2), nil
+			},
+			opt:         RemoveOpt{CompareStrat: IgnoreLabels},
+			expected:    []cleanType(nil),
+			expectedErr: nil,
+		},
+		{
+			name: "multiple receiver, multiple delete, overlap labels",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1, gvr2a, gvr2b}, labels1), nil
+			},
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr2a, gvr2b}, labels1), nil
+			},
+			expected:    []cleanType{{gvr: gvr1, labels: labels1}},
+			expectedErr: nil,
+		},
+		{
+			name: "multiple receiver, multiple delete, different labels",
+			reciever: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr1, gvr2a, gvr2b}, labels1), nil
+			},
+			deletion: func(mapper meta.RESTMapper) ([]cleanType, error) {
+				return addLabels([]schema.GroupVersionResource{gvr2a, gvr2b}, labels2), nil
+			},
+			expected:    []cleanType{{gvr: gvr1, labels: labels1}, {gvr: gvr2a, labels: labels1}, {gvr: gvr2b, labels: labels1}},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := test.reciever.Remove(test.deletion, test.opt)(testMapper{})
+		if err != test.expectedErr {
+			t.Error(
+				"For", test.name,
+				"got", err,
+				"expected", test.expectedErr,
+			)
+		}
+
+		if !reflect.DeepEqual(got, test.expected) {
+			t.Error(
+				"For", test.name,
+				"got", got,
+				"expected", test.expected,
+			)
+		}
+	}
+}
+
+func obj(gvk schema.GroupVersionKind, labels map[string]string) client.Object {
+	o := &unstructured.Unstructured{}
+	o.SetLabels(labels)
+	o.SetGroupVersionKind(gvk)
+	return o
 }
