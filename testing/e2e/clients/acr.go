83c74dcbdbdca4185fc9bd7c4115a27016ebd8dc
Oliver King <kingoliver@microsoft.com>
2023-09-27 14:55:34 -0500

    add resource group

--- testing/e2e/clients/acr.go
+++ testing/e2e/clients/acr.go
@@ -106,7 +106,7 @@ func (a *acr) BuildAndPush(ctx context.Context, imageName, dockerfilePath string
 		// Ideally, we'd use the sdk to build and push the image but I couldn't get it working.
 		// I matched everything on the az cli but wasn't able to get it working with the sdk.
 		// https://github.com/Azure/azure-cli/blob/5f9a8fa25cc1c980ebe5e034bd419c95a1c578e2/src/azure-cli/azure/cli/command_modules/acr/build.py#L25
-		cmd := exec.Command("az", "acr", "build", "--registry", a.name, "--image", imageName, "--subscription", a.subscriptionId, dockerfilePath)
+		cmd := exec.Command("az", "acr", "build", "--registry", a.name, "--image", imageName, "--subscription", a.subscriptionId, "--resource-group", a.resourceGroup, dockerfilePath)
 		cmd.Stdout = newLogWriter(lgr, "building and pushing acr image: ", nil)
 		var errLog bytes.Buffer
 		cmd.Stderr = io.MultiWriter(&errLog, newLogWriter(lgr, "building and pushing acr image: ", to.Ptr(slog.LevelError)))
