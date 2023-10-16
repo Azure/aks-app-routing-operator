30c1162e9255ab29f1260fe3e9ec09c26417f89c
asgayle <asgayle@microsoft.com>
2023-10-16 13:39:54 -0400

    ignore not found error on successful cluster delete

--- pkg/controller/keyvault/ingress_secret_provider_class.go
+++ pkg/controller/keyvault/ingress_secret_provider_class.go
@@ -125,7 +125,7 @@ func (i *IngressSecretProviderClassReconciler) Reconcile(ctx context.Context, re
 	if len(spc.Labels) != 0 && manifests.HasTopLevelLabels(spc.Labels) {
 		logger.Info("removing secret provider class for ingress")
 		err = i.client.Delete(ctx, spc)
-		return result, err
+		return result, client.IgnoreNotFound(err)
 	}
 
 	return result, nil
