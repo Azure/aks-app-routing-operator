58a3a96a18476dca739c091e5b6ea77afb975726
Oliver King <kingoliver@microsoft.com>
2023-10-31 14:27:11 -0500

    add extra comment on crd path

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -43,7 +43,7 @@ func init() {
 	flag.StringVar(&Flags.OperatorDeployment, "operator-deployment", "app-routing-operator", "name of the operator's k8s deployment")
 	flag.StringVar(&Flags.ClusterUid, "cluster-uid", "", "unique identifier of the cluster the add-on belongs to")
 	flag.DurationVar(&Flags.DnsSyncInterval, "dns-sync-interval", defaultDnsSyncInterval, "interval at which to sync DNS records")
-	flag.StringVar(&Flags.CrdPath, "crd", "/crd", "location of the CRD manifests")
+	flag.StringVar(&Flags.CrdPath, "crd", "/crd", "location of the CRD manifests. manifests should be directly in this directory, not in a subdirectory")
 }
 
 type DnsZoneConfig struct {
