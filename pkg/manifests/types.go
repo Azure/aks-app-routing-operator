16b24b817c7d29ecbb4794fa73a6d5d594ca1f57
Asa Gayle <asgayle@microsoft.com>
2024-07-17 14:26:05 -0400

    Fixes for changes to ingress config

--- pkg/manifests/types.go
+++ pkg/manifests/types.go
@@ -56,8 +56,9 @@ type NginxIngressConfig struct {
 	ResourceName          string         // name given to all resources
 	IcName                string         // IngressClass name
 	ServiceConfig         *ServiceConfig // service config that specifies details about the LB, defaults if nil
-	DefaultSSLCertificate string         // namespace/name used to create SSL certificate for the default HTTPS server (catch-all)
 	ForceSSLRedirect      bool           // flag to sets all redirects to HTTPS if there is a default TLS certificate (requires DefaultSSLCertificate)
+	DefaultSSLCertificate string         // namespace/name used to create SSL certificate for the default HTTPS server (catch-all)
+	DefaultBackendService string
 	MinReplicas           int32
 	MaxReplicas           int32
 	// TargetCPUUtilizationPercentage is the target average CPU utilization of the Ingress Controller
