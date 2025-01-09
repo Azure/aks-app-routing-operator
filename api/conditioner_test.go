e2ce175590615503ba4c32e2e92e2577a80fa30c
Jaiveer Katariya <jveerkatariya@gmail.com>
2025-01-09 16:01:23 -0500

    fixed conditioner tests

--- api/conditioner_test.go
+++ api/conditioner_test.go
@@ -4,13 +4,32 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type fakeConditioner struct {
+	conditions []metav1.Condition
+	generation int64
+}
+
+func (f *fakeConditioner) GetCondition(t string) *metav1.Condition {
+	return meta.FindStatusCondition(f.conditions, t)
+}
+
+func (f *fakeConditioner) GetConditions() *[]metav1.Condition {
+	return &f.conditions
+
+}
+func (f *fakeConditioner) GetGeneration() int64 {
+	return f.generation
+}
+
 func TestVerifyAndSetCondition(t *testing.T) {
 	// new condition
-	nic := validNginxIngressController()
-	nic.Generation = 456
+	f := fakeConditioner{
+		generation: 456,
+	}
 
 	cond := metav1.Condition{
 		Type:    "test",
@@ -19,23 +38,23 @@ func TestVerifyAndSetCondition(t *testing.T) {
 		Message: "message",
 	}
 
-	VerifyAndSetCondition(&nic, cond)
-	got := nic.GetCondition(cond.Type)
+	VerifyAndSetCondition(&f, cond)
+	got := f.GetCondition(cond.Type)
 
 	require.NotNil(t, got)
 	require.Equal(t, cond.Status, got.Status)
-	require.Equal(t, nic.Generation, got.ObservedGeneration)
+	require.Equal(t, f.generation, got.ObservedGeneration)
 	require.Equal(t, cond.Reason, got.Reason)
 	require.Equal(t, cond.Message, got.Message)
 
 	// set same condition
-	nic.Generation = 789
-	VerifyAndSetCondition(&nic, cond)
-	got = nic.GetCondition(cond.Type)
+	f.generation = 789
+	VerifyAndSetCondition(&f, cond)
+	got = f.GetCondition(cond.Type)
 
 	require.NotNil(t, got)
 	require.Equal(t, cond.Status, got.Status)
-	require.Equal(t, nic.Generation, got.ObservedGeneration)
+	require.Equal(t, f.generation, got.ObservedGeneration)
 	require.Equal(t, cond.Reason, got.Reason)
 	require.Equal(t, cond.Message, got.Message)
 
@@ -44,19 +63,19 @@ func TestVerifyAndSetCondition(t *testing.T) {
 		Type:   "test2",
 		Status: metav1.ConditionTrue,
 	}
-	VerifyAndSetCondition(&nic, cond2)
-	got = nic.GetCondition(cond2.Type)
+	VerifyAndSetCondition(&f, cond2)
+	got = f.GetCondition(cond2.Type)
 	require.NotNil(t, got)
 	require.Equal(t, cond2.Status, got.Status)
-	require.Equal(t, nic.Generation, got.ObservedGeneration)
+	require.Equal(t, f.generation, got.ObservedGeneration)
 	require.Equal(t, cond2.Reason, got.Reason)
 	require.Equal(t, cond2.Message, got.Message)
 
 	// old condition should not be changed
-	got = nic.GetCondition(cond.Type)
+	got = f.GetCondition(cond.Type)
 	require.NotNil(t, got)
 	require.Equal(t, cond.Status, got.Status)
-	require.Equal(t, nic.Generation, got.ObservedGeneration)
+	require.Equal(t, f.generation, got.ObservedGeneration)
 	require.Equal(t, cond.Reason, got.Reason)
 	require.Equal(t, cond.Message, got.Message)
 }
