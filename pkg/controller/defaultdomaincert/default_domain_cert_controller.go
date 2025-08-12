deac9d6fdb0a6741399b3bcbd4625d33ff9bdea0
Mauricio Ferrari <mauferrari@microsoft.com>
2025-08-12 20:05:33 +0000

    deleted second requeue log

--- pkg/controller/defaultdomaincert/default_domain_cert_controller.go
+++ pkg/controller/defaultdomaincert/default_domain_cert_controller.go
@@ -215,7 +215,6 @@ func (d *defaultDomainCertControllerReconciler) sendRotationEvents(ctx context.C
 							Namespace: ddc.Namespace,
 						},
 					})
-					logger.Info("requeueing DefaultDomainCertificate for rotation event", "name", ddc.Name, "namespace", ddc.Namespace)
 				}
 			}
 		}
